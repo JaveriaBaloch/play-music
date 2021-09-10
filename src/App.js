5586605e0b479fd21d66dfd500d9eb5026483a87
JaveriaBaloch <streetback666@gmail.com>
2021-09-10 21:21:05 +0500

    server isnot answering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import { Component } from "react";
 
 
 class App extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            query:""
+        }
+    }
+    resultShow(){
+        console.log(this.state.query)
+        const value=this.state.query.split(" ")
+        const mixing=value.join("+")
+        console.log(mixing)
+        const URL=`https://spotify-api-wrapper.appspot.com/artist/${mixing}`
+        console.log(URL)
+    }
 render(){
     return(
         <div className="bg-dark text-white">
@@ -10,10 +24,20 @@ render(){
             <h1 className="h1 py-4">Play Music</h1>
                <div className="container">
                <div className="row g-0 my-5">
-                <div className="col-6 ms-auto">
-               <input className="form-control form-control-lg rounded-0 border-0" placeholder="Search your favourite Artist..."/>
+                <div className="col-6  ms-auto">
+               <input className="form-control form-control-lg rounded-0 border-0" placeholder="Search your favourite Artist..."
+               onChange={event=>{this.setState({
+                   query:event.target.value
+               })}}
+               onKeyPress={event=>
+                   {if(event.key==="Enter"){
+                       this.resultShow()
+               }
+            }}
+               />
                </div>
-                <button className="btn btn-warning col-1 me-auto rounded-0">
+                <button className="btn btn-warning col-lg-1 col-2 me-auto rounded-0" onClick={()=>this.resultShow()}>
+                    
                     <i class="fa fa-search"></i></button>
                 </div>
                 <div className="row g-3 my-5 py-5">
